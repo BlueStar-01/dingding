3a8f230a636a817e7573b01174c6d31995dc5a89
碧蓝小新星 <2745672197@qq.com>
2024-12-17 21:29:36 +0800

    章节基础获取

--- dingding-server/src/main/java/com/heima/dingding/collection/ChapterController.java
+++ dingding-server/src/main/java/com/heima/dingding/collection/ChapterController.java
@@ -1,9 +1,15 @@
 package com.heima.dingding.collection;
 
+import com.heima.dingdign.pojo.dto.ChapterDto;
+import com.heima.dingdign.pojo.entity.Book;
 import com.heima.dingdign.pojo.entity.Chapter;
+import com.heima.dingding.constant.MessageConstant;
 import com.heima.dingding.domain.Result;
+import com.heima.dingding.exception.DataException;
+import com.heima.dingding.service.IBookService;
 import com.heima.dingding.service.IChapterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,15 +17,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/chapter")
 @RequiredArgsConstructor
+@Slf4j
 public class ChapterController {
     private final IChapterService chapterService;
+    private final IBookService bookService;
+
 
     /**
      * 根据书籍id获得所有的章节
+     *
      * @return
      */
     @GetMapping("/{bookId}")
     public Result<List<Chapter>> getChapters(@PathVariable Long bookId) {
+        //查询书籍是否存在
+        Long count = bookService.lambdaQuery().eq(Book::getId, bookId).count();
+        if (count <= 0) {
+            throw new DataException(MessageConstant.BOOK_NOT_FOUND);
+        }
         //先返回所有的实体对象
         List<Chapter> list = chapterService.lambdaQuery()
                 .eq(Chapter::getBookId, bookId)
@@ -29,15 +44,33 @@ public class ChapterController {
 
     /**
      * 根据书籍id添加章节
-     * @param bookId
+     *
+     * @param chapter
      * @return
      */
     @PutMapping("/{bookId}")
-    public Result addChapters(@RequestBody Chapter chapter, @PathVariable Long bookId) {
+    public Result addChapters(@RequestBody ChapterDto chapter) {
+        if (chapter.getTitle() == null || chapter.getContent() == null || chapter.getBookId() == null) {
+            throw new DataException(MessageConstant.REQUEST_PARAMETERS_ARE_MISSING);
+        }
+        //查询书籍是否存在
+        Long count = bookService.lambdaQuery().eq(Book::getId, chapter.getBookId()).count();
+        if (count <= 0) {
+            throw new DataException(MessageConstant.BOOK_NOT_FOUND);
+        }
         //添加章节
-        chapter.setBookId(bookId);
-        boolean saved = chapterService.save(chapter);
-        return Result.success(saved);
+        Chapter saveChapter = null;
+        //log.info("{}", chapter);
+        saveChapter = new Chapter();
+        saveChapter.setBookId(chapter.getBookId());
+        saveChapter.setTitle(chapter.getTitle());
+        saveChapter.setContent(chapter.getContent());
+        Integer orderNum = Math.toIntExact(chapter.getOrderNum() != null ? chapter.getOrderNum() :
+                chapterService.lambdaQuery().eq(Chapter::getBookId, chapter.getBookId()).count());
+        saveChapter.setOrderNum(orderNum);
+        //检查属性
+        chapterService.save(saveChapter);
+        return Result.success();
     }
 
 }
--- dingding-server/src/main/java/com/heima/dingding/collection/OrderController.java
+++ dingding-server/src/main/java/com/heima/dingding/collection/OrderController.java
@@ -2,6 +2,8 @@ package com.heima.dingding.collection;
 
 import com.heima.dingdign.pojo.entity.Orders;
 import com.heima.dingdign.pojo.vo.OrderDetailVO;
+import com.heima.dingding.constant.MessageConstant;
+import com.heima.dingding.context.BaseContext;
 import com.heima.dingding.domain.Result;
 import com.heima.dingding.service.IOrdersService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,20 @@ import java.util.List;
 public class OrderController {
 
     private final IOrdersService ordersService;
+
+    /**
+     * 返回当前用户的所有订单
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<Orders>> list() {
+        List<Orders> orders = ordersService.lambdaQuery().eq(Orders::getUserId, BaseContext.getCurrentId()).list();
+        if (orders == null || orders.isEmpty()) {
+            return Result.success(MessageConstant.COL_NOT_FOUND, null);
+        }
+        return Result.success(orders);
+    }
 
 
     /**
